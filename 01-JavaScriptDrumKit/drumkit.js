c13d30c2dae731872ea0ed14c84be58e54e5dfb9
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-03 22:47:46 -0500

    modified:   01-JavaScriptDrumKit/drumkit.js 	modified:   01-JavaScriptDrumKit/index-START.html

--- 01-JavaScriptDrumKit/drumkit.js
+++ 01-JavaScriptDrumKit/drumkit.js
@@ -1,3 +1,20 @@
-/*window.addEventListener('keydown', function(e) {
-    console.log(e);
-})*/
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if (!audio) return; //stop function from running.
+    key.classList.add('playing');
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    console.log(key);
+  }
+  
+  function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip if not a transform
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  window.addEventListener('keydown', playSound);
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
